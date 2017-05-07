98b5e2fc3c58d61e76a7d2eed22bd4cfa5afeb01
yamanakahirofumi <mokojarasi@gmail.com>
2017-05-07 13:48:40 +0900

    arrengement

--- Scraping4blog/Scraping4blog.py
+++ Scraping4blog/Scraping4blog.py
@@ -1,26 +1,25 @@
 from bs4 import BeautifulSoup
-import urllib.request as req
+import requests
 
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, Text, MetaData
-from sqlalchemy.orm import mapper,sessionmaker
+from sqlalchemy import Table, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
+
+import sys,os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../util')
+from LogicBase import LogicBase
 
 Base = declarative_base()
 
-class Scraping4blog:
+class Scraping4blog(LogicBase):
     def run(self):
         # blog URL
         url = 'http://techblog.netflix.com/feeds/posts/default'
-        res = req.urlopen(url)
+        res = requests.get(url)
 
-        soup = BeautifulSoup(res, "html.parser")
+        soup = BeautifulSoup(res.text, "html.parser")
         titlelist = soup.select('feed entry')
-        sendtitle =''
 
-        Session = sessionmaker(bind=self.engine)
-        session = Session()
-        try:
+        with self.sessionmanager(self.Session) as session:
             for entry in titlelist:
                 body = BeautifulSoup(entry.content.string, "html.parser")
                 obj = Blog(entry.id.string, entry.title.string, body.get_text())
@@ -29,16 +28,9 @@ class Scraping4blog:
             for row in session.query(Blog).all():
                 print(row)
                 print(len(row.original_body))
-        except:
-            import sys
-            print(sys.exc_info())
-            print("ダメぽよ")
-            session.rollback
-        finally:
-            session.close()
- 
-    def __init__(self):
-        self.engine = create_engine("sqlite:///:memory:")
+
+    def __init__(self, conf):
+        super().__init__(conf)
         Base.metadata.create_all(self.engine)
 
 
@@ -49,8 +41,8 @@ class Blog(Base):
     title = Column(String)
     original_body = Column(Text)
 
-    def __init__(self, id, title, body):
-        self.blog_id = id
+    def __init__(self, blog_id, title, body):
+        self.blog_id = blog_id
         self.title = title
         self.original_body = body
 
--- Scraping4blog/run.py
+++ Scraping4blog/run.py
@@ -2,8 +2,13 @@
 
 from Scraping4blog import Scraping4blog
 
+import sys,os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../util')
+from settings import SettingManager
+
 def main():
-    instance = Scraping4blog()
+    conf = SettingManager()
+    instance = Scraping4blog(conf)
     instance.run()
 
 if __name__ == "__main__":
