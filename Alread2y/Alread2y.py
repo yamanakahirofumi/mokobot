ed03189b20e6e6f3db5ed6e0653ae181038ff407
yamanakahirofumi <mokojarasi@gmail.com>
2017-05-07 12:57:02 +0900

    fix codacy's issues

--- Alread2y/Alread2y.py
+++ Alread2y/Alread2y.py
@@ -1,32 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from traceback import format_tb
-
 import requests
 import json
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import DBAPIError
-
 from Read import Base, Category, Article
 
+import sys,os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../util')
+from LogicBase import LogicBase
 
-class Alread2y:
+class Alread2y(LogicBase):
     def get(self,cat,name):
-        session = self.Session()
-        try:
+        with self.sessionmanager(self.Session) as session:
             arts = session.query(Article).join(Article.category).filter(Article.read_count == 0, Category.category == cat, Category.name == name).all()
-        except DBAPIError as e:
-            print("ErrorMessage:{0}".format(e.args))
-            print(format_tb(e.__traceback__))
-        finally:
-            session.close()
-        return [ art.ref_id for art in arts ]
-    
+            resultset = [ art.ref_id for art in arts ]
+        return resultset
+
     def update(self, cat, name):
-        session = self.Session()
-        try:
+        with self.sessionmanager(self.Session) as session:
             cat = session.query(Category).filter(Category.category == cat, Category.name == name).first()
             if cat:
                 res = requests.get(cat.getUrl())
@@ -35,47 +26,26 @@ class Alread2y:
                 for art in arts :
                     if art not in sources :
                         cat.articles.append(Article(art))
-                session.commit()
-        except DBAPIError as e:
-            print("ErrorMessage:{0}".format(e.args))
-            print(format_tb(e.__traceback__))
-        finally:
-            session.close()
 
     def regist(self, cat, name, baseurl):
-        session = self.Session()
-        try:
+        with self.sessionmanager(self.Session) as session:
             result = session.query(Category).filter(Category.category == cat, Category.name == name).first()
             if not result :
                 cat = Category(cat, name, baseurl)
                 session.add(cat)
-                session.commit()
-        except DBAPIError as e:
-            print("ErrorMessage:{0}".format(e.args))
-            print(format_tb(e.__traceback__))
-        finally:
-            session.close()
         self.update(cat, name)
         return
 
     def read(self, cat, name, ref_id):
-        session = self.Session()
-        try:
+        with self.sessionmanager(self.Session) as session:
             art = session.query(Article).join(Article.category).filter(Article.ref_id == ref_id, Category.category == cat, Category.name == name).first()
             art.read_count += 1
-        except DBAPIError as e:
-            print("ErrorMessage:{0}".format(e.args))
-            print(format_tb(e.__traceback__))
-        finally:
-            session.close()
-        return num
 
     def run(self):
         pass
         # cat = Category('twitter', 'jenkinsci', '')
 
     def __init__(self, conf):
-        self.engine = create_engine(conf.properties['db']['Urls'], echo=True)
+        super().__init__(conf)
         Base.metadata.create_all(self.engine)
-        self.Session = sessionmaker(bind=self.engine)
 
--- Alread2y/run.py
+++ Alread2y/run.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from Alread2y import Alread2y
+from flask import Flask, jsonify, request, abort
+
+import sys,os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../util')
 from settings import SettingManager
 
-from flask import Flask, jsonify, request, abort
 api = Flask(__name__)
 api.config['JSON_AS_ASCII'] = False
 
-conf = SettingManager()
-
-instance = Alread2y(conf)
+instance = {}
 
 @api.route("/<cat>/<art>", methods=['POST'])
 def checkArticle(cat,art):
@@ -25,16 +26,18 @@ def regist(cat,art):
     return "", 200
 
 @api.route("/<cat>/<art>", methods=['GET'])
-def list(cat,art):
+def listup(cat,art):
     list = instance.get(cat, art)
     body = {"list" : list ,"count" : len(list)}
     return jsonify(body)
 
 
 def main():
+    global instance
+    conf = SettingManager()
+    instance = Alread2y(conf)
     api.run(host=conf.properties['server']['host'],
      port=conf.properties['server']['port'])
-    instance.run()
 
 if __name__ == "__main__":
     main()
